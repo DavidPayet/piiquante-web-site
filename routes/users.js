943cc46612a0f6ce947c9bb476576cf7449539e0
David Payet <davidpayet570@gmail.com>
2022-06-18 17:52:35 +0200

    Email validator

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const userCtrl = require('../controllers/user')
+const userCtrl = require('../controllers/users')
 const password = require('../middlewares/password')
+const checkEmail = require('../middlewares/emailValidator')
 
-router.post('/signup', password, userCtrl.signup)
+router.post('/signup', password, checkEmail, userCtrl.signup)
 router.post('/login', userCtrl.login)
 
 module.exports = router
